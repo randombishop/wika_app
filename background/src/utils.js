1fe560fc5a75250da01a7cf0df6623e211368d89
Nabil Abdellaoui <nabil.abdellaoui@expr-research.com>
2022-05-18 15:28:18 -0400

    Packaging (#5)
    * Simplify build scripts
    
    * typo
    
    * ignore copied files

--- background/src/utils.js
+++
@@ -1,9 +0,0 @@
-function getEnvironment() {
-    const url = window.location.href
-    const env = (url.split(':')[0] === 'chrome-extension') ? 'ext' : 'app'
-    return env ;
-}
-
-export {
-    getEnvironment
-} ;
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './commons/components/App';
-import './commons/css/wika.css';
-import './commons/css/awesome.css';
+import App from './ux/components/App';
+import './ux/css/wika.css';
+import './ux/css/awesome.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
