fb0a58f3a898277118d6450191fa643050a22901
randombishop <nabil.abdellaoui@expr-research.com>
2022-05-19 12:49:45 -0400

    trying v3

---
+++ webapp/src/background/service.js
@@ -0,0 +1,9 @@
+const defaultNetworkType = "Wika Testnet" ;
+const defaultNetworkUrl = "wss://testnode3.wika.network:443" ;
+
+//window.BACKGROUND.initialize(defaultNetworkType, defaultNetworkUrl, () => {
+//        console.log('BACKGROUND init done.')
+//    }) ;
+
+
+//window.BACKGROUND.port = new ExtensionPort() ;
